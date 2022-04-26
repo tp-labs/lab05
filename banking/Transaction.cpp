238764995315e6a056611feedb9e97f438b780d4
Zniker <kirillzolotarev111@mail.ru>
2022-04-26 04:32:16 +0300

    Update Transaction.cpp

--- banking/Transaction.cpp
+++ banking/Transaction.cpp
@@ -36,7 +36,7 @@ bool Transaction::Make(Account& from, Account& to, int sum) {
 
   Credit(to, sum);
 
-  bool success = Debit(to, sum + fee_);
+  bool success = Debit(from, sum + fee_);
   if (!success) to.ChangeBalance(-sum);
 
   SaveToDataBase(from, to, sum);
