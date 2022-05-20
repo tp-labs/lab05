4d7c4d0efa35da788c104ac62c7d633ff6e6eb29
Leonard <artem.vecherinin@mail.ru>
2022-05-20 21:43:20 +0300

    addind submodul

---
+++ banking/_build/CMakeFiles/banking.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/banking.dir/Account.cpp.o"
+  "CMakeFiles/banking.dir/Account.cpp.o.d"
+  "CMakeFiles/banking.dir/Transaction.cpp.o"
+  "CMakeFiles/banking.dir/Transaction.cpp.o.d"
+  "libbanking.a"
+  "libbanking.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/banking.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ banking/_build/CMakeFiles/banking.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for banking.
+# This may be replaced when dependencies are built.
