2994f5be4021e26bff1f2371afc5dc510b0ad714
yourfavoriteself <xeniacuzmina@yandex.ru>
2023-03-15 12:15:04 +0300

    super-puper new

--- include/print.hpp
+++
@@ -1,6 +0,0 @@
-#include <fstream>
-#include <iostream>
-#include <string>
-
-void print(const std::string& text, std::ofstream& out);
-void print(const std::string& text, std::ostream& out = std::cout);
